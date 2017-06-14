85b5b2cbba0b9d17ce4c0e2a3ece4896040af6f6
Tomás <tomas.peirotti@gmail.com>
2017-06-14 17:10:43 -0300

    elimino UserServiceInterface

--- src/main/java/com/oktrueque/service/UserService.java
+++ src/main/java/com/oktrueque/service/UserService.java
@@ -1,16 +1,38 @@
 package com.oktrueque.service;
 
 import com.oktrueque.model.User;
+import com.oktrueque.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Felipe on 7/5/2017.
  */
-public interface UserService {
+@Service
+public class UserService {
 
-    List<User> getUsers();
+    private UserRepository userRepository;
 
-    List<User> getUserById(Long id);
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public List<User> getUsers() {
+        List<User> users = new ArrayList<>();
+        userRepository.findAll().forEach(users :: add);
+        return users;
+    }
+
+    public List<User> getUserById(Long id) {
+        return userRepository.findUserById(id);
+    }
+
+
+
 
 }
