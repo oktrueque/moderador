a11277e1e4aa8988c81d298e9f144fcc4872b46c
Tomás <tomas.peirotti@gmail.com>
2017-10-07 00:25:26 -0300

    Dashboard beggins

--- src/main/java/com/oktrueque/controller/HomeController.java
+++ src/main/java/com/oktrueque/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.oktrueque.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,5 +15,13 @@ public class HomeController {
     @RequestMapping(method = RequestMethod.GET, value = "/")
     public String getHomePage(){
         return "home";
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET, value="/reportes")
+    public ResponseEntity getReportData(){
+
+
+        return new ResponseEntity(HttpStatus.OK);
     }
 }
